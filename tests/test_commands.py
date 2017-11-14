1e9965b920453123066298e911435d20e6287925
Denis Bobrov <delneg@Yandex.ru>
2017-11-14 14:05:06 +0300

    Added test_commands (failing for now)

---
+++ tests/test_commands.py
@@ -0,0 +1,94 @@
+import datetime
+
+from django.core.management import call_command
+from django.test import TestCase
+from django.test.utils import override_settings
+from django.utils.timezone import now
+
+from django_mail_admin.models import OutgoingEmail, STATUS
+
+
+class CommandTest(TestCase):
+    def test_cleanup_mail(self):
+        """
+        The ``cleanup_mail`` command deletes mails older than a specified
+        amount of days
+        """
+        self.assertEqual(OutgoingEmail.objects.count(), 0)
+
+        # The command shouldn't delete today's email
+        email = OutgoingEmail.objects.create(from_email='from@example.com',
+                                             to=['to@example.com'])
+        call_command('cleanup_email', days=30)
+        self.assertEqual(OutgoingEmail.objects.count(), 1)
+
+        # Email older than 30 days should be deleted
+        email.created = now() - datetime.timedelta(31)
+        email.save()
+        call_command('cleanup_email', days=30)
+        self.assertEqual(OutgoingEmail.objects.count(), 0)
+
+    TEST_SETTINGS = {
+        'BACKENDS': {
+            'default': 'django.core.mail.backends.dummy.EmailBackend',
+        },
+        'BATCH_SIZE': 1
+    }
+
+    @override_settings(POST_OFFICE=TEST_SETTINGS)
+    def test_send_queued_mail(self):
+        """
+        Ensure that ``send_queued_mail`` behaves properly and sends all queued
+        emails in two batches.
+        """
+        # Make sure that send_queued_mail with empty queue does not raise error
+        call_command('send_queued_mail', processes=1)
+
+        OutgoingEmail.objects.create(from_email='from@example.com',
+                                     to=['to@example.com'], status=STATUS.queued)
+        OutgoingEmail.objects.create(from_email='from@example.com',
+                                     to=['to@example.com'], status=STATUS.queued)
+        call_command('send_queued_mail', processes=1)
+        self.assertEqual(OutgoingEmail.objects.filter(status=STATUS.sent).count(), 2)
+        self.assertEqual(OutgoingEmail.objects.filter(status=STATUS.queued).count(), 0)
+
+    def test_successful_deliveries_logging(self):
+        """
+        Successful deliveries are only logged when log_level is 2.
+        """
+        email = OutgoingEmail.objects.create(from_email='from@example.com',
+                                             to=['to@example.com'], status=STATUS.queued)
+        call_command('send_queued_mail', log_level=0)
+        self.assertEqual(email.logs.count(), 0)
+
+        email = OutgoingEmail.objects.create(from_email='from@example.com',
+                                             to=['to@example.com'], status=STATUS.queued)
+        call_command('send_queued_mail', log_level=1)
+        self.assertEqual(email.logs.count(), 0)
+
+        email = OutgoingEmail.objects.create(from_email='from@example.com',
+                                             to=['to@example.com'], status=STATUS.queued)
+        call_command('send_queued_mail', log_level=2)
+        self.assertEqual(email.logs.count(), 1)
+
+    def test_failed_deliveries_logging(self):
+        """
+        Failed deliveries are logged when log_level is 1 and 2.
+        """
+        email = OutgoingEmail.objects.create(from_email='from@example.com',
+                                             to=['to@example.com'], status=STATUS.queued,
+                                             backend_alias='error')
+        call_command('send_queued_mail', log_level=0)
+        self.assertEqual(email.logs.count(), 0)
+
+        email = OutgoingEmail.objects.create(from_email='from@example.com',
+                                             to=['to@example.com'], status=STATUS.queued,
+                                             backend_alias='error')
+        call_command('send_queued_mail', log_level=1)
+        self.assertEqual(email.logs.count(), 1)
+
+        email = OutgoingEmail.objects.create(from_email='from@example.com',
+                                             to=['to@example.com'], status=STATUS.queued,
+                                             backend_alias='error')
+        call_command('send_queued_mail', log_level=2)
+        self.assertEqual(email.logs.count(), 1)
