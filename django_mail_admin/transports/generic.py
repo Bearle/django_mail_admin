c3fdc2194edf80f0a218ab96f01d58aaa81f5e1d
Denis Bobrov <delneg@Yandex.ru>
2017-11-17 12:38:03 +0300

    Fixed get_instance, get_message in transports/generic (found with tests ,thx tests!) Added test_mailbox.py Added messages (generated from tests) to gitignore

--- django_mail_admin/transports/generic.py
+++ django_mail_admin/transports/generic.py
@@ -11,18 +11,16 @@ class GenericFileMailbox(EmailTransport):
         super(GenericFileMailbox, self).__init__()
         self._path = path
 
+    def get_instance(self):
+        return self._variant(self._path)
 
-def get_instance(self):
-    return self._variant(self._path)
-
-
-def get_message(self, condition=None):
-    repository = self.get_instance()
-    repository.lock()
-    for key, message in repository.items():
-        if condition and not condition(message):
-            continue
-        repository.remove(key)
-        yield message
-    repository.flush()
-    repository.unlock()
+    def get_message(self, condition=None):
+        repository = self.get_instance()
+        repository.lock()
+        for key, message in repository.items():
+            if condition and not condition(message):
+                continue
+            repository.remove(key)
+            yield message
+        repository.flush()
+        repository.unlock()
---
+++ tests/test_mailbox.py
@@ -0,0 +1,33 @@
+import os
+
+from django.test import TestCase
+
+from django_mail_admin.models import Mailbox
+
+
+class TestMailbox(TestCase):
+    def test_protocol_info(self):
+        mailbox = Mailbox()
+        mailbox.uri = 'alpha://test.com'
+
+        expected_protocol = 'alpha'
+        actual_protocol = mailbox._protocol_info.scheme
+
+        self.assertEqual(
+            expected_protocol,
+            actual_protocol,
+        )
+
+    def test_last_polling_field_exists(self):
+        mailbox = Mailbox()
+        self.assertTrue(hasattr(mailbox, 'last_polling'))
+
+    def test_get_new_mail_update_last_polling(self):
+        mailbox = Mailbox.objects.create(uri="mbox://" + os.path.join(
+            os.path.dirname(__file__),
+            'messages',
+            'generic_message.eml',
+        ))
+        self.assertEqual(mailbox.last_polling, None)
+        mailbox.get_new_mail()
+        self.assertNotEqual(mailbox.last_polling, None)
