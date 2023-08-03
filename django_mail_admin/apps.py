f0ef744b0af7df69952cfe9afbea127979fee92a
Pavel Zagrebelin <zagrebelin@swisstronik.com>
2023-08-03 16:09:33 +0500

    Lock PrimaryKey field type to AutoField.

--- django_mail_admin/apps.py
+++ django_mail_admin/apps.py
@@ -6,3 +6,4 @@ from django.utils.translation import gettext_lazy as _
 class DjangoMailAdminConfig(AppConfig):
     name = 'django_mail_admin'
     verbose_name = _('Mail Admin')
+    default_auto_field = 'django.db.models.AutoField'
